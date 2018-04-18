b0abbf2c0b0fdc9700d8c95afe60a973621996ac
Samuel Gomes Huarachi <samuel.huarachi@gmail.com>
2018-04-17 22:24:25 -0300

    learn redux

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import booksReducer from './reducer_books';
+
+
+const rootReducer = combineReducers({
+  books: booksReducer
+});
+
+
+export default rootReducer;
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'Javascript: The God Parts'},
+        {title: 'Harry Potter'},
+        {title: 'The Dark Tower'},
+        {title: 'Eloquent Ruby'}
+    ]
+}
